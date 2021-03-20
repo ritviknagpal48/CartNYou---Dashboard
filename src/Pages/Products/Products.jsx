34e36b00a6538b298fa30d2e05d0004cd08b2bf9
Durgesh Pandey <heartbeatanna1608@gmail.com>
2021-03-20 12:59:43 +0530

    Updated Product table data and structure

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,5 +1,7 @@
 import ProductTable from "./ProductTable"
 
+import dummy_data from './products.db.json'
+
 const classes = {
   wrapper: "",
   header: "w-full px-36 py-3 bg-white flex flex-row items-center justify-between",
@@ -28,6 +30,14 @@ const Products = () => {
         <div className={classes.title}>Products</div>
         <div className={classes.buttons}>
           <ActionButton
+            title={'Import Inventory'}
+            icon={
+              <svg className={classes.action_icons} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+              </svg>
+            }
+          />
+          <ActionButton
             title={'Export'}
             icon={
               <svg className={classes.action_icons} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -44,18 +54,18 @@ const Products = () => {
             }
           />
           <ActionButton
-            title={'Delete'}
-            icon={
-              <svg className={classes.action_icons} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-              </svg>
-            }
-          />
-          <ActionButton
             title={'Add'}
             icon={
               <svg className={classes.action_icons} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+              </svg>
+            }
+          />
+          <ActionButton
+            title={'Filters'}
+            icon={
+              <svg className={classes.action_icons} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
               </svg>
             }
           />
@@ -64,7 +74,8 @@ const Products = () => {
 
       <div>
         <ProductTable
-          heading={['Name', 'Title', 'Status', 'Role', 'Action']}
+          heading={Object.keys(dummy_data[0])}
+          rows={dummy_data}
         />
       </div>
     </div>
