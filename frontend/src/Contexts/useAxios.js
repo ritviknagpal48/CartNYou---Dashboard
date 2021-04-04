6af22e5c71e56a9f96fe41d4bf92275ae378daf5
Durgesh Pandey <heartbeatanna1608@gmail.com>
2021-04-05 01:48:29 +0530

    Auth Enabled with API connection

---
+++ frontend/src/Contexts/useAxios.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const useAxios = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  axiosInstance.interceptors.request.use(
+    (req) => {
+      setIsLoading(true);
+      setError(null);
+      return req;
+    },
+    (err) => {
+      setIsLoading(false);
+      setError(err);
+      return err;
+    }
+  );
+
+  axiosInstance.interceptors.response.use(
+    (res) => {
+      setIsLoading(false);
+      setError(null);
+      return res;
+    },
+    (err) => {
+      setIsLoading(false);
+      setError(err);
+      return err;
+    }
+  );
+
+  return { axios: axiosInstance, isLoading, error };
+};
+
+export default useAxios;
--- frontend/src/Pages/Auth/Login.jsx
+++ frontend/src/Pages/Auth/Login.jsx
@@ -1,8 +1,11 @@
 // @ts-nocheck
-import clsx from "clsx";
-import { AuthContext, AUTH_ACTIONS } from "Contexts/Auth";
 import { useContext, useState } from "react";
+import { Spin, message } from "antd";
+import clsx from "clsx";
 import { Link, useHistory } from "react-router-dom";
+
+import { AuthContext, AUTH_ACTIONS } from "Contexts/Auth";
+import useAxios from "Contexts/useAxios";
 
 const classes = {
   wrapper: "",
@@ -21,26 +24,60 @@ const classes = {
   remember_me: "mb-4 ring-none"
 };
 
+const capitalize = (value) => `${value}`.split('').map((x, idx) => idx === 0 ? x.toUpperCase() : x.toLowerCase()).join('')
+
 const Login = ({ className }) => {
 
   const history = useHistory()
   const { setAuth } = useContext(AuthContext)
 
   const [userType, setUserType] = useState("wholeseller")
+  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("")
+
+  const { axios, isLoading } = useAxios()
 
   const signIn = (e) => {
     e.preventDefault();
-    setAuth(AUTH_ACTIONS.LOGIN, {
-      isLoggedIn: true,
-      token: "some JWT token here",
-      user: {
-        fname: "Test",
-        username: "Test",
-        type: userType
-      },
-      additionalInfo: {},
+
+    axios.post('http://localhost:1337/auth/local', {
+      identifier: username,
+      password: password
+    }).then(res => {
+      if (!res.data) {
+        return message.error(`No ${capitalize(userType)} found with given credentials.`, 1)
+      }
+
+      const { jwt, user } = res.data;
+
+      if (user.type !== userType) {
+        return message.error(`No ${capitalize(userType)} found with given credentials.`, 1)
+      }
+      if (user.isBlocked) {
+        return message.error('This account is Blocked. Please contact Support Team for more information.', 1)
+      }
+
+      message.success(`Welcome Back, ${user.username}`, 1)
+      axios.defaults.headers = {
+        'authorization': `Bearer ${jwt}`
+      }
+      setAuth(AUTH_ACTIONS.LOGIN, {
+        isLoggedIn: true,
+        token: jwt,
+        user: {
+          fname: user.username,
+          username: user.username,
+          email: user.email,
+          type: userType
+        },
+        additionalInfo: {
+          ...user
+        },
+      })
+      history.push(`/${userType}/dashboard`)
+    }).catch(err => {
+      message.error(err.message, 1);
     })
-    history.push(`/${userType}/dashboard`)
   };
 
   return (
@@ -49,77 +86,83 @@ const Login = ({ className }) => {
         <div className={classes.logo}>CartNYou</div>
         <h2 className={classes.title}>Login</h2>
         <div className={classes.divider} />
-        <div className={classes.row}>
-          <div className={classes.row_item}>
-            <input type="radio" name="type" id="wholeseller" className={'mr-1 text-red-500'} checked={userType === 'wholeseller'} onChange={(e) => setUserType(p => e.target.checked ? e.target.id : p)} />
-            <label htmlFor="wholeseller">WholeSeller</label>
+        <Spin spinning={isLoading} size={'large'}>
+          <div className={classes.row}>
+            <div className={classes.row_item}>
+              <input type="radio" name="type" id="wholeseller" className={'mr-1 text-red-500'} checked={userType === 'wholeseller'} onChange={(e) => setUserType(p => e.target.checked ? e.target.id : p)} />
+              <label htmlFor="wholeseller">WholeSeller</label>
+            </div>
+            <div className={classes.row_item}>
+              <input type="radio" name="type" id="retailer" className={'mr-1 text-red-500'} checked={userType === 'retailer'} onChange={(e) => setUserType(p => e.target.checked ? e.target.id : p)} />
+              <label htmlFor="retailer">Retailer</label>
+            </div>
           </div>
-          <div className={classes.row_item}>
-            <input type="radio" name="type" id="retailer" className={'mr-1 text-red-500'} checked={userType === 'retailer'} onChange={(e) => setUserType(p => e.target.checked ? e.target.id : p)} />
-            <label htmlFor="retailer">Retailer</label>
+          <div>
+            <label htmlFor="email-address" className="sr-only">
+              Username
+            </label>
+            <input
+              id="email-address"
+              name="email"
+              type="text"
+              autoComplete="username"
+              required
+              className={classes.input}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
           </div>
-        </div>
-        <div>
-          <label htmlFor="email-address" className="sr-only">
-            Email address
-          </label>
-          <input
-            id="email-address"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            className={classes.input}
-            placeholder="Email address"
-          />
-        </div>
-        <div>
-          <label htmlFor="password" className="sr-only">
-            Password
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="none"
-            required
-            className={classes.input}
-            placeholder="Password"
-          />
-        </div>
-        <div className={classes.remember_me}>
-          <input type="checkbox" name="remember-me" id="remember-me" className={'mr-1 text-red-500 border-red-500'} />
-          <label htmlFor="remember-me">Remember Me</label>
-        </div>
-        <button
-          type="submit"
-          className={classes.submit_button}
-          onClick={signIn}
-        >
-          <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-            <svg
-              className="h-5 w-5 text-red-500 group-hover:text-red-400"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                clipRule="evenodd"
-              />
-            </svg>
+          <div>
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="none"
+              required
+              className={classes.input}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className={classes.remember_me}>
+            <input type="checkbox" name="remember-me" id="remember-me" className={'mr-1 text-red-500 border-red-500'} />
+            <label htmlFor="remember-me">Remember Me</label>
+          </div>
+          <button
+            type="submit"
+            className={classes.submit_button}
+            onClick={signIn}
+          >
+            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg
+                className="h-5 w-5 text-red-500 group-hover:text-red-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+            Sign in
+          </button>
+          <div className={clsx(classes.divider, "mt-6 mb-4")} />
+          <span className={classes.create_account}>
+            New here? &nbsp;
+            <Link to="/auth/register" className={classes.create_account_inner}>
+              Register Now
+            </Link>
           </span>
-          Sign in
-        </button>
-        <div className={clsx(classes.divider, "mt-6 mb-4")} />
-        <span className={classes.create_account}>
-          New here? &nbsp;
-          <Link to="/auth/register" className={classes.create_account_inner}>
-            Register Now
-          </Link>
-        </span>
+        </Spin>
       </div>
     </div>
   );
