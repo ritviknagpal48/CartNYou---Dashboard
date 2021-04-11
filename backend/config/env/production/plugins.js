5925645fe2fd2e900e8ef2211e78a588333349c2
Durgesh Pandey <heartbeatanna1608@gmail.com>
2021-04-11 18:42:32 +0530

    Updated backend

--- backend/config/env/production/plugins.js
+++ backend/config/env/production/plugins.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   email: {
     provider: "nodemailer",
     providerOptions: {
-      host: env("SMTP_HOST", "smtp.example.com"),
-      port: env("SMTP_PORT", 465),
+      host: env("SMTP_HOST", "smtp.gmail.com"),
+      port: env.int("SMTP_PORT", 465),
       auth: {
         type: "OAuth2",
         accessToken: env("SMTP_ACCESS_TOKEN"),
@@ -14,10 +14,15 @@ module.exports = ({ env }) => ({
       },
       // ... any custom nodemailer options
       secure: true,
+      service: "gmail",
+      from: `"CartNYou Support" <${env("SMTP_USER_EMAIL")}>`,
+      tls: {
+        rejectUnauthorized: false,
+      },
     },
-    // settings: {
-    //   defaultFrom: "hello@example.com",
-    //   defaultReplyTo: "hello@example.com",
-    // },
+    settings: {
+      defaultFrom: env("SMTP_USER_EMAIL"),
+      defaultReplyTo: env("SMTP_USER_EMAIL"),
+    },
   },
 });
